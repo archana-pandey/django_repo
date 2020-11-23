7f2d9c6cc154d1d32deb39de7dd21f4a416f2cee
archana <archanaraj883@gmail.com>
2020-11-23 19:17:09 +0530

    start

---
+++ student/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return render(request,'regform.html')
+
+def studentRegistration(request):
+    name = request.POST.get('name', '')
+    fname = request.POST.get('fname', '')
+    mname = request.POST.get('mname','')
+    dob = request.POST.get('dob','')
+    res = {'name':name, 'fname':fname,'mname':mname,'dob':dob}
+    return render(request,'result.html',{'result':res})
+    
